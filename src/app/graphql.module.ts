476f72c0cb64a21ac51c213f50ecb2c93b05c151
Alan Kanjo <alan.kanjo@swan.de>
2023-10-23 15:19:27 +0200

    cleaning

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -2,9 +2,9 @@ import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { NgModule } from '@angular/core';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
-import { AppComponent } from './app.component';
+import {Constants} from "./constants";
 
-const uri = AppComponent.graphqlUrl; // <-- add the URL of the GraphQL server here
+const uri = Constants.API_GRAPHQL_ENDPOINT; // <-- add the URL of the GraphQL server here
 
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
