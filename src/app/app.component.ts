2b76c5efc2f570f97dc3675b787a3e4a20b014d5
Alan Kanjo <alan.kanjo@swan.de>
2023-10-24 11:14:01 +0200

    changed how app starts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,40 @@
 import {Component, Input, OnInit} from '@angular/core';
 import { CsrfService } from './services/csrf/csrf.service';
 import { Constants } from './constants';
+import {User} from "./models";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  @Input() authenticated: boolean = false;
+  authenticated: boolean | undefined = false;
   title = Constants.TitleOfSite;
   loading = true;
   ready = false;
   error: any;
 
   constructor(
-    private httpService: CsrfService
+    private httpService: CsrfService,
+    public user: User,
   ) {}
 
   async ngOnInit() {
-    try {
-      this.httpService.setCsrfToken();
-      this.loading = false;
-      this.ready = true;
+  try {
+    if (await this.httpService.startSession()) {
+      this.authenticated = this.user.isAuthenticated();
+      //this.httpService.setCsrfToken();
     }
-    catch (error) {
-      this.error = error;
-      this.loading = false;
-      this.ready = false;
-    }
+    this.loading = false;
+    this.ready = true;
   }
+  catch (error) {
+    this.error = error;
+    this.loading = false;
+    this.ready = false;
+  }
+}
+
 
 }
 
--- src/app/services/csrf/csrf.service.ts
+++ src/app/services/csrf/csrf.service.ts
@@ -1,13 +1,13 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map, pipe, throwError, catchError } from 'rxjs';
-import { AppComponent } from '../../app.component';
-import { Constants } from '../../constants';
-import { CookieService } from 'ngx-cookie-service';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {map, pipe, throwError, catchError} from 'rxjs';
+import {AppComponent} from '../../app.component';
+import {Constants} from '../../constants';
+import {CookieService} from 'ngx-cookie-service';
+
 interface CsrfResponse {
   csrfToken: string;
 }
-
 
 
 @Injectable({
@@ -17,7 +17,24 @@ export class CsrfService {
   constructor(
     private http: HttpClient,
     private cookieService: CookieService,
-    ) {}
+  ) {
+  }
+
+  async startSession(): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+      this.http.get(Constants.API_GRAPHQL_ENDPOINT).subscribe({
+          next: (result: any) => {
+            resolve(result.ok)
+          },
+          error: (error) => {
+            console.log('Error starting session', error);
+            reject('Failed to start session, please try again later.');
+          }
+        }
+      );
+    });
+  }
+
 
   private getCsrfToken(url: string) {
     return this.http.get<CsrfResponse>(url).pipe(
@@ -25,25 +42,25 @@ export class CsrfService {
         return data.csrfToken;
       }),
       catchError((error) => {
-        console.log('Error fetching CSRF token',error);
+        console.log('Error fetching CSRF token', error);
         return throwError(error);
       })
     );
   }
 
   setCsrfToken(): void {
-  this.getCsrfToken(Constants.API_CSRF_ENDPOINT).subscribe(
-    {
-      next: (data) => {
-        this.cookieService.set(Constants.CSRF_COOKIE_NAME, data);
-      },
-      error: (error) => {
-        console.error('Error setting CSRF token', error);
-        throwError(error);
+    this.getCsrfToken(Constants.API_CSRF_ENDPOINT).subscribe(
+      {
+        next: (data) => {
+          this.cookieService.set(Constants.CSRF_COOKIE_NAME, data);
+        },
+        error: (error) => {
+          console.error('Error setting CSRF token', error);
+          throwError(error);
+        }
       }
-    }
-  )
-}
+    )
+  }
 
 
 }
