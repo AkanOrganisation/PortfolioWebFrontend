5ac0eff0f5bdb3b1ec26e9c0b3d219c0e4dee76d
Alan Kanjo <alan.kanjo@swan.de>
2023-10-28 23:13:53 +0200

    cleaning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {UserModel} from "./models";
-import {BreakpointObserver} from "@angular/cdk/layout";
-import {MatSidenav} from "@angular/material/sidenav";
-import {LinksConstants} from "./constants/links-constants";
 import {ComponentState} from "./constants";
 import {AppInitializerService} from "./services/initializer/app.initializer";
 
@@ -12,15 +9,12 @@ import {AppInitializerService} from "./services/initializer/app.initializer";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  @ViewChild(MatSidenav)
-  sidenav!: MatSidenav;
 
   state = ComponentState.LOADING;
   error: any;
 
   constructor(
     public user: UserModel,
-    private observer: BreakpointObserver,
     private appInitializer: AppInitializerService,
   ) {
   }
@@ -35,19 +29,6 @@ export class AppComponent implements OnInit {
     });
   }
 
-  ngAfterViewInit() {
-    this.observer.observe(["(max-width: 800px)"]).subscribe((res) => {
-      if (res.matches) {
-        this.sidenav.mode = "over";
-        this.sidenav.close();
-      } else {
-        this.sidenav.mode = "side";
-        this.sidenav.open();
-      }
-    });
-  }
-
-  protected readonly Constants = LinksConstants;
   protected readonly ComponentState = ComponentState;
 }
 
