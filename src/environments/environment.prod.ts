0e76e1be3f3122a02b396d7f64ae5ee21bbd6363
Alan Kanjo <alan.kanjo@swan.de>
2023-11-01 13:58:23 +0100

    environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://dhbw-mosbach-web-events-dcf5cbcab3e8.herokuapp.com/', // Your production backend API URL
+    mapsApi: 'AIzaSyDQhi8xOARa-rnMsA10F0NCjw-ohyZmyQs',
+};
