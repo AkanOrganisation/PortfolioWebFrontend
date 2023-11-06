aade537145479919deceba12438435c61d8b4e1e
Alan Kanjo <alan.kanjo@swan.de>
2023-11-06 13:09:08 +0100

    csrf

--- src/app/services/graphql/graphql.module.ts
+++ src/app/services/graphql/graphql.module.ts
@@ -1,10 +1,10 @@
-import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
-import { HttpLink } from 'apollo-angular/http';
-import { NgModule } from '@angular/core';
-import { ApolloClientOptions, InMemoryCache, ApolloLink } from '@apollo/client/core';
-import { HttpHeaders } from '@angular/common/http';
-import { API_ENDPOINTS } from "../../constants";
-import { CookieService } from 'ngx-cookie-service'; // Make sure you have installed ngx-cookie-service
+import {APOLLO_OPTIONS, ApolloModule} from 'apollo-angular';
+import {HttpLink} from 'apollo-angular/http';
+import {NgModule} from '@angular/core';
+import {ApolloClientOptions, InMemoryCache, ApolloLink} from '@apollo/client/core';
+import {HttpHeaders} from '@angular/common/http';
+import {API_ENDPOINTS} from "../../constants";
+import {CookieService} from 'ngx-cookie-service'; // Make sure you have installed ngx-cookie-service
 
 const uri = API_ENDPOINTS.API_GRAPHQL_ENDPOINT;
 
@@ -22,8 +22,8 @@ export function createApollo(httpLink: HttpLink, cookieService: CookieService): 
   const authLink = new ApolloLink((operation, forward) => {
     const csrfToken = getCsrfToken();
 
-    operation.setContext(({ headers = new HttpHeaders() }) => ({
-      headers: headers.append('X-CSRFToken', csrfToken)
+    operation.setContext(({headers = new HttpHeaders()}) => ({
+      headers: headers.append(API_ENDPOINTS.CSRF_HEADER_NAME, csrfToken)
     }));
 
     return forward(operation);
@@ -32,7 +32,6 @@ export function createApollo(httpLink: HttpLink, cookieService: CookieService): 
   return {
     cache: new InMemoryCache(),
     link: authLink.concat(http),
-    credentials: 'include'
   };
 }
 
@@ -46,4 +45,5 @@ export function createApollo(httpLink: HttpLink, cookieService: CookieService): 
     },
   ],
 })
-export class GraphQLModule {}
+export class GraphQLModule {
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:8000/', // Your local backend API URL
+  apiUrl: 'http://127.0.0.2:8000/', // Your local backend API URL
   mapsApi : 'AIzaSyCeWc7ME81mb-b2XooTV-8OG-LvPzyRwt4',
 };
