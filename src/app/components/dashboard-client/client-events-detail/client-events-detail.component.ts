8d139bb9c85d508715d3eef1fdb3a1ce07b360c0
Alan Kanjo <alan.kanjo@swan.de>
2023-11-08 02:23:45 +0100

    added address as subtitle

--- src/app/components/dashboard-client/client-events-detail/client-events-detail.component.ts
+++ src/app/components/dashboard-client/client-events-detail/client-events-detail.component.ts
@@ -7,125 +7,134 @@ import {EventModel} from "../../../models/event.models";
 import {UserService} from "../../../services";
 import {ClientModel} from "../../../models";
 import {MatSnackBar} from "@angular/material/snack-bar";
+import {AddressNodeType} from "../../../graphql/location/address.graphql";
 
 @Component({
-    selector: 'app-client-events-detail',
-    templateUrl: './client-events-detail.component.html',
-    styleUrls: ['./client-events-detail.component.css']
+  selector: 'app-client-events-detail',
+  templateUrl: './client-events-detail.component.html',
+  styleUrls: ['./client-events-detail.component.css']
 })
 export class ClientEventsDetailComponent implements OnInit {
-    private subscription: Subscription | undefined;
+  private subscription: Subscription | undefined;
 
-    event: EventNodeType | null = null;
-    state: ComponentState = ComponentState.LOADING;
-    mode: ComponentMode = ComponentMode.HIDE;
+  event: EventNodeType | null = null;
+  state: ComponentState = ComponentState.LOADING;
+  mode: ComponentMode = ComponentMode.HIDE;
 
-    @Output() close = new EventEmitter<void>();
-    @Output() success = new EventEmitter<void>();
-
-
-    minDate: Date = new Date();
+  @Output() close = new EventEmitter<void>();
+  @Output() success = new EventEmitter<void>();
 
 
-    selectedDate: Date | null = null;
-    selectedDateTime: EventDateTimeNodeType | null = null;
+  minDate: Date = new Date();
 
-    constructor(
-        private eventModel: EventModel,
-        private userService: UserService,
-        private clientModel: ClientModel,
+
+  selectedDate: Date | null = null;
+  selectedDateTime: EventDateTimeNodeType | null = null;
+
+  constructor(
+    private eventModel: EventModel,
+    private userService: UserService,
+    private clientModel: ClientModel,
+  ) {
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  loadEvent(event: EventNodeType) {
+    this.state = ComponentState.PROCESSING;
+    this.subscription = this.eventModel.getEventById(event.id).subscribe({
+        next: (result: any) => {
+          this.event = {
+            ...event,
+            ...result.data.eventClientPublic as EventNodeType
+          };
+          this.toggle();
+          this.state = ComponentState.READY;
+
+        },
+        error: (error: any) => {
+          console.error(error);
+          this.state = ComponentState.ERROR;
+        }
+      }
+    );
+  }
+
+
+  get eventDates(): Date[] {
+    return this.event?.dates?.edges
+      .filter((edge) => edge.node.datetime !== undefined)
+      .map((edge) =>
+        new Date(edge.node.datetime as Date)
+      ) ?? [];
+  }
+
+
+  public eventTimes(searchDate: Date | null) {
+    if (!searchDate) {
+      return [];
+    }
+    return this.event?.dates?.edges.filter((edge) =>
+      edge.node.datetime !== undefined &&
+      new Date(edge.node.datetime).toDateString() === searchDate.toDateString()).map(
+      (edge) => edge.node) ?? [];
+  }
+
+  eventDatesFilter = (d: Date | null): boolean => {
+    if (!d) {
+      return false;
+    }
+    return this.eventDates.some(eventDate =>
+      eventDate.toDateString() === d.toDateString()
+    );
+  };
+
+
+  toggle(): void {
+    this.mode = this.mode === ComponentMode.HIDE ? ComponentMode.SHOW : ComponentMode.HIDE;
+    if (this.mode === ComponentMode.HIDE
     ) {
+      this.event = null;
+      if (this.subscription) {
+        this.subscription.unsubscribe();
+      }
+      this.state = ComponentState.COMPLETED;
+      this.close.emit();
     }
+  }
 
-    ngOnInit(): void {
+  readonly ComponentState = ComponentState;
+  readonly ComponentMode = ComponentMode;
 
+  takeEvent() {
+    if (!this.selectedDateTime || !this.selectedDateTime.id) {
+      console.log(this.selectedDateTime)
+      console.error('No date selected');
+      return;
     }
-
-    loadEvent(event: EventNodeType) {
-        this.state = ComponentState.PROCESSING;
-        this.subscription = this.eventModel.getEventById(event.id).subscribe({
-                next: (result: any) => {
-                    this.event = {
-                        ...event,
-                        ...result.data.eventClientPublic as EventNodeType
-                    };
-                    this.toggle();
-                    this.state = ComponentState.READY;
-
-                },
-                error: (error: any) => {
-                    console.error(error);
-                    this.state = ComponentState.ERROR;
-                }
-            }
-        );
-    }
-
-
-    get eventDates(): Date[] {
-        return this.event?.dates?.edges
-            .filter((edge) => edge.node.datetime !== undefined)
-            .map((edge) =>
-                new Date(edge.node.datetime as Date)
-            ) ?? [];
-    }
-
-
-    public eventTimes(searchDate: Date | null) {
-        if (!searchDate) {
-            return [];
+    this.state = ComponentState.PROCESSING;
+    this.clientModel.takeEvent(this.selectedDateTime.id as string).subscribe({
+      next: (result: any) => {
+        this.state = ComponentState.COMPLETED;
+        if (result.data.clientTakeOrExitEvent.success) {
+          this.success.emit()
         }
-        return this.event?.dates?.edges.filter((edge) =>
-            edge.node.datetime !== undefined &&
-            new Date(edge.node.datetime).toDateString() === searchDate.toDateString()).map(
-            (edge) => edge.node) ?? [];
+        this.toggle();
+      },
+      error: (error: any) => {
+        console.error(error);
+        this.state = ComponentState.ERROR;
+      }
+    })
+  }
+
+  formatAddress(address: AddressNodeType | undefined) {
+    if (!address) {
+      return '';
     }
+    return `${address.streetName} ${address.streetNumber}, ${address.city}, ${address.postalCode}, ${address.country}`;
 
-    eventDatesFilter = (d: Date | null): boolean => {
-        if (!d) {
-            return false;
-        }
-        return this.eventDates.some(eventDate =>
-            eventDate.toDateString() === d.toDateString()
-        );
-    };
-
-
-    toggle(): void {
-        this.mode = this.mode === ComponentMode.HIDE ? ComponentMode.SHOW : ComponentMode.HIDE;
-        if (this.mode === ComponentMode.HIDE
-        ) {
-            this.event = null;
-            if (this.subscription) {
-                this.subscription.unsubscribe();
-            }
-            this.state = ComponentState.COMPLETED;
-            this.close.emit();
-        }
-    }
-
-    readonly ComponentState = ComponentState;
-    readonly ComponentMode = ComponentMode;
-
-    takeEvent() {
-        if (!this.selectedDateTime || !this.selectedDateTime.id) {
-            console.log(this.selectedDateTime)
-            console.error('No date selected');
-            return;
-        }
-        this.state = ComponentState.PROCESSING;
-        this.clientModel.takeEvent(this.selectedDateTime.id as string).subscribe({
-            next: (result: any) => {
-                this.state = ComponentState.COMPLETED;
-                if (result.data.clientTakeOrExitEvent.success) {
-                    this.success.emit()
-                }
-                this.toggle();
-            },
-            error: (error: any) => {
-                console.error(error);
-                this.state = ComponentState.ERROR;
-            }
-        })
-    }
+  }
 }
